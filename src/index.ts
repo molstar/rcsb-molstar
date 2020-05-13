5d6857e29342a76b6b9f99af01967491e5b39402
Alexander Rose <alexander.rose@weirdbyte.de>
2020-05-13 10:59:19 -0700

    fix viewer path

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@
 
 import * as path from 'path'
 
-export const getStructureViewerAbsoluteFSPath = function () {
-    return path.resolve(path.join(__dirname, '..', 'dist', 'structure-viewer'))
+export const getRcsbMolstarViewerAbsoluteFSPath = function () {
+    return path.resolve(path.join(__dirname, '..', 'dist', 'viewer'))
 }
